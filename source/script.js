713d1082639ffdf4a7cebfd76cd81bca3cb697a1
Leisan Iusupova <leisan_11_@mail.ru>
2022-04-19 12:15:26 +0300

    Добавляет декоративные элементы

---
+++ source/script.js
@@ -0,0 +1,13 @@
+let headerList = document.querySelector('.main-nav__list');
+let headerToggle = document.querySelector('.header__toggle');
+
+headerToggle.addEventListener('click', function () {
+  if (headerToggle.classList.contains('header__toggle--closed-menu')) {
+    headerToggle.classList.add('header__toggle--open-menu')
+    headerToggle.remove('header__toggle--closed-menu');
+    ;
+  } else {
+    headerToggle.classList.add('header__toggle--closed-menu');
+    headerToggle.classList.remove('header__toggle--open-menu');
+  }
+});
