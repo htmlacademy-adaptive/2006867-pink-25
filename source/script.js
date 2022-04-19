a5ceba7faed7daaaea13231167bbff3461e073f5
Leisan Iusupova <leisan_11_@mail.ru>
2022-04-19 23:45:03 +0300

    Исправляет замечания

--- source/script.js
+++ source/script.js
@@ -4,7 +4,7 @@ let headerToggle = document.querySelector('.header__toggle');
 headerToggle.addEventListener('click', function () {
   if (headerToggle.classList.contains('header__toggle--closed-menu')) {
     headerToggle.classList.add('header__toggle--open-menu')
-    headerToggle.remove('header__toggle--closed-menu');
+    headerToggle.classList.remove('header__toggle--closed-menu');
     ;
   } else {
     headerToggle.classList.add('header__toggle--closed-menu');
