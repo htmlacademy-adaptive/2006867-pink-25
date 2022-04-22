47f421b7b2c8ae990da3ca93f099a5f2649c4bd4
Leisan Iusupova <leisan_11_@mail.ru>
2022-04-23 02:04:52 +0300

    Добавляет исправления

--- source/script.js
+++ source/script.js
@@ -1,13 +1,15 @@
-let headerList = document.querySelector('.main-nav__list');
+let headerList = document.querySelector('.main-nav');
 let headerToggle = document.querySelector('.header__toggle');
+let mainHeader = document.querySelector('.main-header');
+
+
+mainHeader.classList.remove('main-header__no-js');
+headerToggle.classList.remove('header__toggle--no-js');
+headerList.classList.remove('main-nav--nojs');
+
 
 headerToggle.addEventListener('click', function () {
-  if (headerToggle.classList.contains('header__toggle--closed-menu')) {
-    headerToggle.classList.add('header__toggle--open-menu')
-    headerToggle.classList.remove('header__toggle--closed-menu');
-    ;
-  } else {
-    headerToggle.classList.add('header__toggle--closed-menu');
-    headerToggle.classList.remove('header__toggle--open-menu');
-  }
+  headerToggle.classList.toggle('header__toggle--open-menu');
+  headerToggle.classList.toggle('header__toggle--closed-menu');
+  headerList.classList.toggle('main-nav--nojs');
 });
