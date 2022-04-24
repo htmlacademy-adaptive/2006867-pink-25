2a2d34561a3beaaaaee93f15e722bb42cbbffa0c
Leisan Iusupova <leisan_11_@mail.ru>
2022-04-25 01:08:49 +0300

    Добавляет исправления

--- source/script.js
+++ source/script.js
@@ -3,6 +3,7 @@ let headerToggle = document.querySelector('.header__toggle');
 let mainHeader = document.querySelector('.main-header');
 
 
+
 mainHeader.classList.remove('main-header__no-js');
 headerToggle.classList.remove('header__toggle--no-js');
 headerList.classList.remove('main-nav--nojs');
@@ -13,3 +14,8 @@ headerToggle.addEventListener('click', function () {
   headerToggle.classList.toggle('header__toggle--closed-menu');
   headerList.classList.toggle('main-nav--nojs');
 });
+
+window.onload = function() {
+  let serviceTable = document.getElementsByClassName('.service__wrapper');
+  serviceTable[0].scrollTo(0, 100);
+}
