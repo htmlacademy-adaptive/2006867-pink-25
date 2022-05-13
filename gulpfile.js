792fbf0b3c550e021098a65dd874dcecdb67dce4
Leisan Iusupova <leisan_11_@mail.ru>
2022-05-14 00:23:06 +0300

    Добавляет исправления

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,15 @@ import sass from 'gulp-dart-sass';
 import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
 import browser from 'browser-sync';
+import Webp from 'gulp-webp';
+import imageMin from 'gulp-libsquoosh';
+import htmlmin from 'gulp-htmlmin';
+import scripts from 'gulp-terser';
+
 
 // Styles
 
-export const styles = () => {
+const styles = () => {
   return gulp.src('source/sass/style.scss', { sourcemaps: true })
     .pipe(plumber())
     .pipe(sass().on('error', sass.logError))
@@ -17,6 +22,61 @@ export const styles = () => {
     .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
     .pipe(browser.stream());
 }
+
+// HTML
+const html= () => {
+  return gulp.src("build/*.html")
+  .pipe(htmlmin({ collapseWhitespace: true }))
+  .pipe(gulp.dest("build"));
+  }
+
+// SVG
+
+const svg = () => {
+  return gulp.src(['source/img/.svg', '!source/img/icons/.svg'])
+  .pipe(svgo())
+  .pipe(gulp.dest('build/img'));
+  const sprite = () => {
+  return gulp.src('source/img/icons/*.svg')
+  .pipe(svgo())
+  .pipe(svgstore({
+  inlineSvg: true
+  }))
+  .pipe(rename('sprite.svg'))
+  .pipe(gulp.dest('build/img'));
+  }
+
+//WebP
+
+const createWebp = () => {
+  return gulp.src('source/img/**/*.{png,jpg}')
+    .pipe(squoosh({
+    webp: {}
+    }))
+    .pipe(gulp.dest('build/img'))
+}
+
+// Images
+
+const optimizeImages = () => {
+  return gulp.src('source/img/**/*.{png,jpg}')
+  .pipe(squoosh())
+  .pipe(gulp.dest('build/img'))
+  }
+
+const copyImages = () => {
+  return gulp.src('source/img/**/*.{png,jpg}')
+  .pipe(gulp.dest('build/img'))
+}
+
+// Scripts
+
+const scripts = () => {
+  return gulp.src('source/js/*.js')
+  .pipe(terser())
+  .pipe(gulp.dest('build/js'))
+  }
+
 
 // Server
 
--- source/js/script.js
+++ source/js/script.js
@@ -6,14 +6,14 @@ let mainHeader = document.querySelector('.main-header');
 
 mainHeader.classList.remove('main-header__no-js');
 headerToggle.classList.remove('header__toggle--no-js');
-headerList.classList.remove('main-nav--nojs');
+headerList.classList.remove('main-nav__no-js');
 
 
 
 headerToggle.addEventListener('click', function () {
   headerToggle.classList.toggle('header__toggle--open-menu');
   headerToggle.classList.toggle('header__toggle--closed-menu');
-  headerList.classList.toggle('main-nav--nojs');
-  mainHeader.classList.toggle('main-header--open-menu');
+  headerList.classList.toggle('main-nav__no-js');
+  mainHeader.classList.toggle('main-header__open-menu');
 });
 
