9d912c18f9cce340bbf96e3f6fc17fc736092366
Leisan Iusupova <leisan_11_@mail.ru>
2022-05-17 23:34:46 +0300

    Добавляет исправления

--- source/js/script.js
+++ source/js/script.js
@@ -4,15 +4,15 @@ let mainHeader = document.querySelector('.main-header');
 
 
 
-mainHeader.classList.remove('main-header__no-js');
+mainHeader.classList.remove('main-header--no-js');
 headerToggle.classList.remove('header__toggle--no-js');
-headerList.classList.remove('main-nav__no-js');
+headerList.classList.remove('main-nav--no-js');
 
 
 
 headerToggle.addEventListener('click', function () {
   headerToggle.classList.toggle('header__toggle--open-menu');
   headerToggle.classList.toggle('header__toggle--closed-menu');
-  headerList.classList.toggle('main-nav__no-js');
-  mainHeader.classList.toggle('main-header__open-menu');
+  headerList.classList.toggle('main-nav--no-js');
+  mainHeader.classList.toggle('main-header--open-menu');
 });
